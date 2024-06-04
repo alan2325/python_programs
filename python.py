bdc9f4afcb36f08356b01dced9eaeafc91182db7
alan2325 <alalkumar06@gmail.com>
2024-06-04 11:33:07 +0530

    python

---
+++ python.py
@@ -0,0 +1,39 @@
+# print('welcome','','')
+# age=20
+# name="Alan"
+# weight=62.5
+
+# print('Age is:',age) 
+# print('Name is:',name)
+# print('Weight is:',weight)
+
+
+# a=10
+# print(type(a))
+# b=20.5
+# print(type(b))
+# c=2+5j
+# print(type(c))
+# name="alan"
+# print(type(name))
+# d=True
+# print(type(d))
+# e=False
+# print(type(e))
+# l=[10,20]
+# print(type(l))
+# t=(10,20)
+# print(type(t))
+# s={10,20}
+# print(type(s))
+# f={'name':'hlo'}
+# print(type(f))
+
+
+age=int(input('enter age :'))
+name=str(input('Enter name :'))
+weight=float(input('Enter weight :'))
+
+print('Age :',age)
+print('Name :',name)
+print('weignt:',weight)

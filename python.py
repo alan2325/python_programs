848c555d94a328aa94563507c6213a7941db38f3
alan2325 <alalkumar06@gmail.com>
2024-06-05 11:16:33 +0530

    done

--- python.py
+++ python.py
@@ -30,10 +30,81 @@
 # print(type(f))
 
 
-age=int(input('enter age :'))
-name=str(input('Enter name :'))
-weight=float(input('Enter weight :'))
+# age=int(input('enter age :'))
+# name=str(input('Enter name :'))
+# weight=float(input('Enter weight :'))
 
-print('Age :',age)
-print('Name :',name)
-print('weignt:',weight)
+# print('Age :',age)
+# print('Name :',name)
+# print('weignt:',weight)
+
+ 
+# print('Arithmetical')
+# print(10+5)
+# print(5-3)
+# print(10*5)
+# print(10/5)
+# print(10%2)
+# print(10**3)
+# print(10//5)
+
+# print('comparison')
+# print(10==10,10==11)
+# print(10!=10,10!=11)
+# print(10>5,10>11)
+# print(10<5,10<11)
+# print(10>=5,10>=11)
+# print(10<=10,10<=9)
+
+# print('assignment')
+# a=10
+# print(a)
+# a=20
+# a+=10
+# print(a)
+# a=20
+# a-=10
+# print(a)
+# a=20
+# a*=10
+# print(a)
+# a=20
+# a/=10
+# print(a)
+# a=20
+# a**=3
+# print(a)
+# a=20
+# a//=10
+# print(a)
+
+# print('logical')
+# print(10==10 and 10>=8)
+# print(10==11 or 10>=8)
+# print(not(10==10))
+
+# print('membership')
+# a='welcome'
+# print('e'in a)
+# print('z'in a)
+# print('e'not in a)
+# print('z'not in a)
+
+print('identity')
+a=10
+b=10
+print(id(a))
+print(id(b))
+
+print(a is b)
+print(a is not b)
+
+
+a=[10,20]
+b=[10,20]
+
+print(id(a))
+print(id(b))
+
+print(a is b)
+print(a is not b)

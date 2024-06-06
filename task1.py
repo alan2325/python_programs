97b29b8ca272081738e0f628b305c78b23ea7fc0
alan2325 <alalkumar06@gmail.com>
2024-06-06 11:10:53 +0530

    done

---
+++ task1.py
@@ -0,0 +1,34 @@
+print('sum')
+a=int(input('Enter one numbers :'))
+b=int(input('Enter another numbers :'))
+print('Sum is :',a+b)
+
+print('mul')
+c=int(input('Enter one numbers :'))
+d=int(input('Enter another numbers :'))
+print('mul is :',c*d)
+
+print('sub')
+a=int(input('Enter one numbers :'))
+b=int(input('Enter another numbers :'))
+print('sub is :',a-b)
+
+print('mod')
+a=int(input('Enter one numbers :'))
+b=int(input('Enter another numbers :'))
+print('mod is :',a/b)
+
+print('div')
+a=int(input('Enter one numbers :'))
+b=int(input('Enter another numbers :'))
+print('div is :',a%b)
+
+a=int(input('Enter one numbers :'))
+b=int(input('Enter another numbers :'))
+print('ans is :',a**b)
+
+a=int(input('Enter one numbers :'))
+b=int(input('Enter another numbers :'))
+print('ans is :',a//b)
+
+
---
+++ task2.py
@@ -0,0 +1,19 @@
+# a="welcome"
+# print(a[0])
+
+# print(a[-6])
+
+# print(len(a))
+
+# print(a[0:4])
+
+# print(a[3:7])
+
+# print(a[ : ])
+# print(a[: :-1])
+# print(a[::2])
+
+
+a=str(input('Enter item :'))
+b=int(input('Enter price :'))
+print(f"Item is {a} and its price is{b}")

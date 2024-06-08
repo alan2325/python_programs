db8739734bca2382863a7784573134fa13f88c08
alan2325 <alalkumar06@gmail.com>
2024-06-09 00:43:05 +0530

    done

--- task3.py
+++ task3.py
@@ -1,15 +1,14 @@
 print('___Welcome___')
-salary = float(input("Enter your net salary : RS "))
-years_of_service = int(input("Enter your years of service : "))
+year = int(input("How many years service do you have ? : "))
 
-
-if years_of_service >5:
+if year >5:
+    salary = float(input("Enter your net salary : RS "))
     
     bonus_amount = 0.05 * salary
 
     print("Congratulations! You are eligible for a bonus.")
     print("Your net bonus amount is : RS ",bonus_amount)
-    print("Your total amount is : RS",bonus_amount+salary)
+    print("Your total amount is : RS ",bonus_amount+salary)
 
 else:
 

a30ad196bed130dd657d74e5c3f11d4d50cb5e5d
alan2325 <alalkumar06@gmail.com>
2024-06-11 22:28:49 +0530

    done

---
+++ task9.py
@@ -0,0 +1,17 @@
+a=int(input("Enter first number "))
+b=int(input("Enter second number "))
+c=int(input("Enter third number "))
+
+if a>b:
+    if a>c:
+        print( "Greatest number is : ",a)
+
+    else:
+        print("Greatest number is : ",c)
+
+else:
+    if b>c:
+        print("Greatest number is : ",b)
+
+    else:
+        print("Greatest number is : ",c)

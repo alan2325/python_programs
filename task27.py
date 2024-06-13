5d4578b7fe1fa359b33fcbc1dc6c4522e2d53bd0
alan2325 <alalkumar06@gmail.com>
2024-06-13 11:24:34 +0530

    done

---
+++ task27.py
@@ -0,0 +1,6 @@
+a = str(input("Enter word : "))
+rev=""
+
+for i in a:
+    rev=i+rev
+print("Reverse is : ",rev)
---
+++ task28.py
@@ -0,0 +1,7 @@
+rows = 3
+cols = 3
+
+for i in range(rows):
+    for j in range(cols):
+        print(' *',end='')
+    print()

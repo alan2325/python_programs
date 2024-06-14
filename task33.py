7b5ae9c9597ac0875196a09b47dbaea3a5f78b0d
alan2325 <alalkumar06@gmail.com>
2024-06-14 11:28:39 +0530

    done

--- task33.py
+++ task33.py
@@ -10,7 +10,15 @@
 #     print()        
 
 
-for i in range(1,4):
-    for j in range(i,0,-1):
-        print(j,end=" ")
-    print()        
+# for i in range(1,4):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()        
+
+a=0
+for i  in range(1,4):
+    for j in range(i):
+        print(a,end=" ")
+        a +=1
+    print()
+

64c8073028484da947caf20be773e3ecfb6abcff
alan2325 <alalkumar06@gmail.com>
2024-06-14 11:13:47 +0530

    done

---
+++ task33.py
@@ -0,0 +1,16 @@
+# for i in range(1,4):
+#     for j in range(1,1+i):
+#         print(j,end=" ")
+#     print()
+
+
+# for i in range(1,4):
+#     for j in range(1,1+i):
+#         print(i,end=" ")
+#     print()        
+
+
+for i in range(1,4):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()        

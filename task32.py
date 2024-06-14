e453d695638c14a813ec2796b7063f1622f5bf24
alan2325 <alalkumar06@gmail.com>
2024-06-14 10:46:02 +0530

    done

---
+++ task32.py
@@ -0,0 +1,40 @@
+# for i in range(3):
+#     for j in range(4):
+#         print(j,end=" ")
+#     print()    
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print(i,end=" ")
+#     print()        
+
+# a=0
+# for i in range(3):
+#     for j in range(3):
+#         print(a,end=" ")
+#         a+=1
+#     print()
+
+
+# for i in range(4):
+#     a=2
+#     for j in range(3):
+#         if i%2==0:
+#             print(j,end=" ")
+#         else:
+#             print(a,end=" ")
+#             a-=1
+#     print()        
+
+for i in range(4):
+    for j in range(4):
+        print(i+j,end=" ")
+    print()       
+
+a=0
+for i in range(3):
+    for j in range(3):
+        a+=2
+        print(a,end=" ")
+    print()    

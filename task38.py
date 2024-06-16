8e557dc287614d1b901a1210a9fc107a44e6b332
alan2325 <alalkumar06@gmail.com>
2024-06-16 22:50:41 +0530

    done

--- task38.py
+++ task38.py
@@ -1,4 +1,9 @@
-a,b=0,1
-for i in range(8):
-    print(a,end=" ")
-    a,b=b,a+b
+# a,b=0,1
+# for i in range(8):
+#     print(a,end=" ")
+#     a,b=b,a+b
+
+# for i in range(6):
+#     print(i * i, end=" ")
+
+

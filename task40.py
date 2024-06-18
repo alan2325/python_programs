16b390f0ce97eabb9ab174efb1875af30143ab01
alan2325 <alalkumar06@gmail.com>
2024-06-18 10:29:20 +0530

    done

---
+++ task40.py
@@ -0,0 +1,49 @@
+
+# a=int(input("enter starting number : "))
+# b=int(input("enter ending number : "))
+# while a<=b:
+#     print(a)
+#     a+=1
+
+# sum of n mun
+
+# a=int(input("enter starting number : "))
+# b=int(input("enter ending number : "))
+# sum=0
+# while a<=b:
+#     sum+=a
+#     a+=1
+# print(sum)
+
+    # odd number
+
+# a=int(input("enter starting number : "))
+# b=int(input("enter ending number : "))
+# while a<=b:
+#     if a%2==1:
+#         print(a)
+#     a+=1
+
+
+    # even number
+# a=int(input("enter starting number : "))
+# b=int(input("enter ending number : "))
+# while a<=b:
+#     if a%2==0:
+#         print(a)
+#     a+=1
+
+
+
+# sum of even
+
+a=int(input("enter starting number : "))
+b=int(input("enter ending number : "))
+sum=0
+while a<=b:
+    if a%2==1:
+        print(a)
+        sum+=a
+    a+=1
+print("sum :",sum)
+   

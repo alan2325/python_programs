8dda7784abe94b9bd424501dd3b1e0ca6dbb9745
alan2325 <alalkumar06@gmail.com>
2024-06-18 11:06:44 +0530

    done

--- task40.py
+++ task40.py
@@ -37,35 +37,44 @@
 
 # sum of even
 
-a=int(input("enter starting number : "))
-b=int(input("enter ending number : "))
-sum=0
-while a<=b:
-    if a%2==1:
-        print(a)
-        sum+=a
-    a+=1
-print("sum :",sum)
+# a=int(input("enter starting number : "))
+# b=int(input("enter ending number : "))
+# sum=0
+# while a<=b:
+#     if a%2==1:
+#         print(a)
+#         sum+=a
+#     a+=1
+# print("sum :",sum)
 
 
 # sum of even odd nat
 
-a=int(input("Enter starting value : "))
-b=int(input("Enter ending value : "))
+# a=int(input("Enter starting value : "))
+# b=int(input("Enter ending value : "))
 
-sum=0 
-odd=0
-even=0
-while a<=b:
-    sum+=a
+# sum=0 
+# odd=0
+# even=0
+# while a<=b:
+#     sum+=a
 
-    if a%2==1:
-        odd+=a
+#     if a%2==1:
+#         odd+=a
     
-    else:
-        even+=a
-    a+=1
-print("sum of even : ",even)
-print("sum of odd: ",odd)
-print("sum of natural : ",sum)      
+#     else:
+#         even+=a
+#     a+=1
+# print("sum of even : ",even)
+# print("sum of odd: ",odd)
+# print("sum of natural : ",sum)      
    
+# factorial of a no
+
+i=1
+a=int(input("Enter a number : "))
+fat=1
+while i<=a: 
+    fat*=i
+    i+=1
+print(fat)

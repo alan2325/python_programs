30cede44ce5df050c01c24fbbd44922926a694e2
alan2325 <alalkumar06@gmail.com>
2024-06-19 23:02:02 +0530

    done

--- task41.py
+++ task41.py
@@ -1,5 +1,15 @@
-a=int(input('enter a value'))
-i=1
-while i<=10:
-    print(i,'*',a,'=',a*i)
-    i+=1
+# a=int(input('enter a value'))
+# i=1
+# while i<=10:
+#     print(i,'*',a,'=',a*i)
+#     i+=1
+
+
+
+a=int(input('enter a number'))
+rev=0
+while a>0:
+    d=a%10
+    rev=(rev*10)+d
+    a//=10
+print(rev)

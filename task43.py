f6f3fa7f5cf8a400babe9d6d4a51fba7778ce25a
alan2325 <alalkumar06@gmail.com>
2024-06-20 10:34:22 +0530

    done

--- task43.py
+++ task43.py
@@ -1,6 +1,14 @@
     # continue
 
+# for i in range(10):
+#     if i==5:
+#         continue
+#     print(i)
+
+
+    # pass
+
 for i in range(10):
     if i==5:
-        continue
+        pass
     print(i)

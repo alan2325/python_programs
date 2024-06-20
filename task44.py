1395dd6dd6332aeca673f486ec029319387c5b50
alan2325 <alalkumar06@gmail.com>
2024-06-20 11:31:27 +0530

    done

--- task44.py
+++ task44.py
@@ -26,9 +26,30 @@
 
     # deletion
 
-L=[1,2,'welcome',7.5,1,2]
-print(L[2])
-L[2]="helo"
-print(L)
-del L[2]
+# L=[1,2,'welcome',7.5,1,2]
+# print(L[2])
+# L[2]="helo"
+# print(L)
+# del L[2]
+# print(L)
+
+
+    # list add
+
+# L=[1,2,3,4]
+    # append
+# L.append(20)
+    # insert
+# L.insert(3,7)
+    # extend
+# L.extend([100,200])
+# print(L)
+
+L=[10,20,[11,12]]
+    remove
+L.remove([11,12])
+    pop
+L.pop()
+    clear
+# L.clear()
 print(L)

759b7e5a3ccca402b6696275d702bf76ee6fcea9
alan2325 <alalkumar06@gmail.com>
2024-06-20 10:52:44 +0530

    done

---
+++ task44.py
@@ -0,0 +1,34 @@
+    # list
+
+
+# L=[1,2,'welcome',7.5,1,2]
+# print(L[2])
+
+# if 2 in L:
+#     print("in l")
+# else:
+#     print(" not in l ")
+# for i in L:
+#     print(i)
+# print(type(L))
+# print(len(L))
+
+
+    # updation
+
+# L=[1,2,'welcome',7.5,1,2]
+# print(L[2])
+# L[2]="helo"
+# print(L)
+# L[2]="come"
+# print(L)
+
+
+    # deletion
+
+L=[1,2,'welcome',7.5,1,2]
+print(L[2])
+L[2]="helo"
+print(L)
+del L[2]
+print(L)

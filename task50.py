2af468dd049ece2aa1789a4bdd3067388bcc8769
alan2325 <alalkumar06@gmail.com>
2024-06-23 22:43:40 +0530

    python

--- task50.py
+++ task50.py
@@ -1,18 +1,22 @@
-# Define the list
+# print largest number of a list using swap
+
 numbers = [5, 3, 8, 6, 2, 9, 1]
-
-# Print the list
 print("List of numbers:", numbers)
-
-# Find the largest number using a swapping method
 for i in range(len(numbers) - 1):
     for j in range(i + 1, len(numbers)):
         if numbers[i] < numbers[j]:
-            # Swap numbers[i] and numbers[j]
             temp = numbers[i]
             numbers[i] = numbers[j]
             numbers[j] = temp
-
-# The largest number is now the first element
 largest_number = numbers[0]
-print("Largest number in the list:", largest_number)
+print("Largest number in the list:", largest_number)
+
+                 #or
+
+numbers = [15, 22, 8, 19, 42, 3, 89, 16, 56, 1]
+print(numbers)
+largest_number = numbers[0]
+for i in range(1, len(numbers)):
+    if numbers[i] > largest_number:
+        largest_number = numbers[i]
+print("The largest number in the list is:",largest_number)

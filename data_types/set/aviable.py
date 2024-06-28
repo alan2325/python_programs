607cd36d6d749d376549b5626b3cca7ee9edf3ea
alan2325 <alalkumar06@gmail.com>
2024-06-28 11:35:01 +0530

    done

---
+++ data_types/set/aviable.py
@@ -0,0 +1,13 @@
+s={1,2,3,4}
+print(s)
+
+    # aviable
+
+
+a=int(input("Enter a value : "))
+if a in s:
+    print("Present")
+else:
+    print("Absent ")
+
+
--- data_types/set/task1.py
+++ data_types/set/task1.py
@@ -1,13 +1,14 @@
-s={1,2,3,4}
-print(s)
+php=set()
+bio=set()
+c=int(input("Enter your choice \n1:php \n2:bio"))
 
-    # aviable
-
-
-a=int(input("Enter a value : "))
-if a in s:
-    print("Present")
-else:
-    print("Absent ")
-
-
+l_b=str(input("Enter the number of student in bio : "))
+while True:
+    if c==1:
+        l_p=str(input("Enter the number of student in php : "))
+        for i in range(l_p):
+            php_nm=str(input("Enter the name of students : "))
+    if c==2:
+        l_b=str(input("Enter the number of student in bio : "))
+        for i in range(l_b):
+            bio_nm=str(input("Enter the name of students : "))

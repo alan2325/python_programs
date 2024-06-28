5b09e2a011bce573c25e92a0d59f8fe4796b2d2b
alan2325 <alalkumar06@gmail.com>
2024-06-28 23:39:54 +0530

    python

--- data_types/set/task1.py
+++ data_types/set/task1.py
@@ -1,14 +1,26 @@
 php=set()
 bio=set()
-c=int(input("Enter your choice \n1:php \n2:bio"))
-
-l_b=str(input("Enter the number of student in bio : "))
 while True:
+    c=int(input('1.Add students to biology and chemistry\n2.view students\n3.View common students \n4.Exit \nEnter your choice : '))
     if c==1:
-        l_p=str(input("Enter the number of student in php : "))
-        for i in range(l_p):
-            php_nm=str(input("Enter the name of students : "))
-    if c==2:
-        l_b=str(input("Enter the number of student in bio : "))
-        for i in range(l_b):
-            bio_nm=str(input("Enter the name of students : "))
+        p=int(input("Enter the number of student in php : "))
+        for i in range(p):
+            php_nm=str(input("Enter the name of students in php : "))
+            php.add(php_nm)
+
+        b=int(input("Enter the number of student in bio : "))
+        for i in range(b):
+            bio_nm=str(input("Enter the name of students in bio : "))
+            bio.add(bio_nm)
+    
+    elif c==2:
+        print('Students in php :',php)
+        print('Students in biology :',bio)
+    elif c==3:
+        print('common students are :')
+        print(php.intersection(bio))
+    elif c==4:
+        print("You had exited")
+        break
+    else:
+        print("Invalid input")

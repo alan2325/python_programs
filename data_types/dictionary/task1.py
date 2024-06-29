ec4dd153a8e092604432d15ec9ef0d8e7b4e7668
alan2325 <alalkumar06@gmail.com>
2024-06-29 10:05:43 +0530

    done

---
+++ data_types/dictionary/task1.py
@@ -0,0 +1,15 @@
+d={}
+std={'name':'alal','age':20,'place':'vkm'}
+print(std['name'])
+
+    # updation
+std['name']='manu'
+print(std['name'])
+
+    # add key(field)
+std['mark']=100
+print(std['mark'])
+
+    # print key & values
+for i in std:
+    print(i,std[i])

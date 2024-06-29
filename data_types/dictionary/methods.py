ea82a2851513dd5d2eadbfba2818a6c8ccf3072f
alan2325 <alalkumar06@gmail.com>
2024-06-29 10:53:31 +0530

    done

---
+++ data_types/dictionary/methods.py
@@ -0,0 +1,43 @@
+d={'name':'alal','age':20}  
+l=[1,2,3,4] 
+
+
+    # 1.get
+# print(d.get('name'))
+
+    # 2.item
+# print(d.items())
+
+    # 3. keys
+# print(d.keys())
+
+    # 4. values
+# print(d.values())
+
+#     5.clear
+# d.clear()
+# print(d)
+
+#     6.copy
+t=d.copy()
+print(t)
+
+#     7.pop
+# d.pop("age")
+# print(d)
+
+#     8.popitem
+# d.popitem()
+# print(d)
+
+#     9.setdefault
+# d.setdefault("place")
+# print(d)
+
+#     10.update
+# d.update({'place':'vkm'})
+# print(d)
+
+#     11.fromkey
+# d1=d.fromkeys(l)
+# print(d1)

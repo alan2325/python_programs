84f811588069e3f40af19c5f0b33bbfb77e0ae99
alan2325 <alalkumar06@gmail.com>
2024-07-01 11:05:40 +0530

    done

---
+++ data_types/maping/dictionary/task.py
@@ -0,0 +1,11 @@
+d={}
+std={'name':'alal','age':20,'place':'vkm'}
+print(std['name'])
+
+    # updation
+std['name']='manu'
+print(std['name'])
+
+    # add key(field)
+std['mark']=100
+print(std['mark'])
--- data_types/maping/dictionary/task1.py
+++ data_types/maping/dictionary/task1.py
@@ -1,11 +1,36 @@
-d={}
-std={'name':'alal','age':20,'place':'vkm'}
-print(std['name'])
+l=int(input("Enter the limit : "))
+d=[{'name':'a','age':20,'place':'vkm'},
+   {'name':'b','age':44,'place':'ktm'},
+   {'name':'c','age':50,'place':'ekm'},
+   {'name':'d','age':30,'place':'alp'}]
+for i in range(l):
+    name=str(input("Enter the name : "))
+    age=int(input("Enter the age : "))
+    place=str(input("Enter your place : "))
+    d.append({'name':name,'age':age,'place':place})
+print("{:<10}{:<6}{:<6}".format("name","age","place"))
+print('_'*22)
+for i in d:
+    print("{:<10}{:<6}{:<6}".format(i['name'],i['age'],i['place']))    
+print()
+print("Age above 40 : ")
+print("{:<10}{:<6}{:<6}".format("name","age","place"))
+print('_'*22)    
+for i in d:
+    
+    if i["age"] >=40:
+        
+        print("{:<10}{:<6}{:<6}".format(i['name'],i['age'],i['place']))  
+        
 
-    # updation
-std['name']='manu'
-print(std['name'])
-
-    # add key(field)
-std['mark']=100
-print(std['mark'])
+print()        
+print("Age below 40 : ")
+print("{:<10}{:<6}{:<6}".format("name","age","place"))
+print('_'*22)  
+for i in d:
+    
+    if i["age"] <40:
+      
+        
+        print("{:<10}{:<6}{:<6}".format(i['name'],i['age'],i['place']))   
+        

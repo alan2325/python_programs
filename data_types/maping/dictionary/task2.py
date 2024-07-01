ab727a5036d55a1467fd646e658bea4f8978a3d7
alan2325 <alalkumar06@gmail.com>
2024-07-01 11:30:18 +0530

    done

---
+++ data_types/maping/dictionary/task2.py
@@ -0,0 +1,8 @@
+num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+a=int(input("Enter a number : "))
+for i in range(len(num)):
+    if a in num:
+        i=0
+        # print("Values are : ",num[i])
+        print(i+a)
+   
--- task41.py
+++ task41.py
@@ -46,12 +46,12 @@ print(rev)
 
 
 
-a=0
-b=1
-i=0
-while i<=10:
-    print(a)
-    c=a+b
-    a=b
-    b=c
-    i+=1
+# a=0
+# b=1
+# i=0
+# while i<=10:
+#     print(a)
+#     c=a+b
+#     a=b
+#     b=c
+#     i+=1

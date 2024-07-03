8ebb35b3c0e410dcf3d8d5c6e9f578a1f9f1ddae
alan2325 <alalkumar06@gmail.com>
2024-07-03 10:23:32 +0530

    done

--- function/task1.py
+++ function/task1.py
@@ -7,12 +7,36 @@
 
 
 
-print("helo")
-a=20                #global veriable
-def fun1():
-    a=15            #local veriable
-    a=a+10
-    print("local",a)
+# print("helo")
+# a=20                #global veriable
+# def fun1():
+#     a=15            #local veriable
+#     a=a+10
+#     print("local",a)
 
-print("global",a)
-fun1()
+# print("global",a)
+# fun1()
+
+
+
+             
+# def fun1():
+#     global a
+#     a=15             #global veriable
+#     print("local",a)
+
+# fun1()
+
+# print("global",a)
+
+
+
+
+
+
+def fun1():
+    c=20
+    return c,50
+# print(fun1())
+c1,d1=fun1()
+print(c1,d1)

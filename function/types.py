255ca79ed530dd888c086ef094584cdb8f75c241
alan2325 <alalkumar06@gmail.com>
2024-07-03 10:51:57 +0530

    done

--- function/types.py
+++ function/types.py
@@ -1,5 +1,12 @@
         #positional argument
-def add(a,b):
-    return a+b
-print(add('abc','def'))
-print(add(10,20))
+# def add(a,b):
+#     return a+b
+# print(add('abc','def'))
+# print(add(10,20))
+
+
+
+def add(a,b=20):
+    return a,b
+print(add('abc'))
+print(add('cde',25))

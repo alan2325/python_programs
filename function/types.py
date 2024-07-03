58b70814a1ca9f65dbe391bba87042cab0e3d64c
alan2325 <alalkumar06@gmail.com>
2024-07-03 11:20:44 +0530

    done

--- function/types.py
+++ function/types.py
@@ -1,20 +1,36 @@
-        #positional argument
+            # 1.positional argument
 # def add(a,b):
 #     return a+b
 # print(add('abc','def'))
 # print(add(10,20))
 
 
-
+            # 2.default
 # def add(a,b=20):
 #     return a,b
 # print(add('abc'))
 # print(add('cde',25))
 
 
+            # 3.keyword
+# def add(a,b):
+#     print('name : ',a)
+#     print('age',b)
+# add(b=20,a='abc')
+# add('cde',25)
 
-def add(a,b):
-    print('name : ',a)
-    print('age',b)
-add(b=20,a='abc')
-add('cde',25)
+
+
+            # 4.arbritaty 
+# def add(*a):
+#     return(a)
+# print(add('dheb',23))
+# print(add())
+# print(add(50))
+
+
+            # 5.arbritaty keyword
+def add(**a):
+    return a
+print(add(a='dheb',b=23))
+print(add())

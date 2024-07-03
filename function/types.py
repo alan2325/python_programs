ad54d63aee6fd5ba9ae9656b6735aaf2e74ec390
alan2325 <alalkumar06@gmail.com>
2024-07-03 11:01:28 +0530

    done

--- function/types.py
+++ function/types.py
@@ -6,7 +6,15 @@
 
 
 
-def add(a,b=20):
-    return a,b
-print(add('abc'))
-print(add('cde',25))
+# def add(a,b=20):
+#     return a,b
+# print(add('abc'))
+# print(add('cde',25))
+
+
+
+def add(a,b):
+    print('name : ',a)
+    print('age',b)
+add(b=20,a='abc')
+add('cde',25)

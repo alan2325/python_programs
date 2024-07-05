b69499ab52bc965034dc62f7d0632c6a6485feba
alan2325 <alalkumar06@gmail.com>
2024-07-05 10:34:38 +0530

    done

--- module/mod1.py
+++ module/mod1.py
@@ -1,5 +1,41 @@
-def fun1():
-    print("Welcome")
+# def fun1():
+#     print("Welcome")
 
-def fun2():
-    print("Hello")
+# def fun2():
+#     print("Hello")
+
+
+
+
+
+
+
+    #task
+
+            # 1.positional argument
+# def add(a,b):
+#     return a+b
+
+
+
+            # 2.default
+# def add(a,b=20):
+#     return a,b
+
+
+
+            # 3.keyword
+# def add(a,b):
+#     print('name : ',a)
+#     print('age',b)
+
+
+
+            # 4.arbitrary 
+# def add(*a):
+#     return(a)
+
+
+            # 5.arbitrary keyword
+def add(**a):
+    return a
--- module/mod2.py
+++ module/mod2.py
@@ -20,3 +20,25 @@
 # from mod1 import *
 # fun2()
 # fun1()
+
+
+
+    #task
+
+
+from mod1 import *
+# print(add(10,20))
+# print(add(40,50))
+
+# print(add(b=20,a='abc'))
+# print(add('cde',25))
+ 
+# add(b=20,a='abc')
+# add('cde',25)
+
+# print(add('dheb',23))
+# print(add())
+# print(add(50))
+
+print(add(a='dheb',b=23))
+print(add())

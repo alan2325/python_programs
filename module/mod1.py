ae6f50e504a253b6593474511aa63ae2b1b0b32f
alan2325 <alalkumar06@gmail.com>
2024-07-05 10:10:40 +0530

    done

---
+++ module/mod1.py
@@ -0,0 +1,5 @@
+def fun1():
+    print("Welcome")
+
+def fun2():
+    print("Hello")
---
+++ module/mod2.py
@@ -0,0 +1,22 @@
+#    #1.
+# import mod1
+# mod1.fun1()
+# mod1.fun2()
+
+
+#    #2
+# import mod1 as s
+# s.fun1()
+# s.fun2()
+
+
+#    #3
+# from mod1 import fun1,fun2
+# fun1()
+# fun2()
+
+
+#    #4
+# from mod1 import *
+# fun2()
+# fun1()

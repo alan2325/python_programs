4b3f8b44859cd25f38076d9a891d2676dd30912a
alan2325 <alalkumar06@gmail.com>
2024-07-06 10:24:26 +0530

    done

---
+++ module/task2/dele.py
@@ -0,0 +1,9 @@
+def dele(data):
+    id = input("Enter User ID to delete: ")
+    for i in data:
+            if i["id"] == id:
+                data.remove(i)
+                print("User deleted successfully!\n")
+                break
+    else:
+            print("ID not found!\n")
--- module/task2/task.py
+++ module/task2/task.py
@@ -1,7 +1,9 @@
 from add import *
 from dis import *
 from up import *
-data=[]
+from dele import *
+data=[{"id": "1", "name": "Alice", "age": "30", "place": "New York"},
+    {"id": "2", "name": "Bob", "age": "25", "place": "Chicago"},]
 while True:
     print("1.Add \n2.Display \n3.Update \n4.Delate \n5.Exit")
     ch=int(input("Enter your choice : "))
@@ -11,3 +13,10 @@ while True:
         dis(data) 
     elif ch == 3:
         up(data)
+    elif ch == 4:
+        dele(data)
+    elif ch == 5:
+        print("You had exited")
+        break
+    else:
+        ("Invalid input")

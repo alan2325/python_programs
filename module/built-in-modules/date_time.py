1f4241d34ce9c92d8fab8674910315ef6972f0b7
alan2325 <alalkumar06@gmail.com>
2024-07-08 10:32:36 +0530

    done

---
+++ module/built-in-modules/date_time.py
@@ -0,0 +1,5 @@
+import datetime
+print(datetime.datetime.now().strftime("%x"))
+print(datetime.datetime.now().strftime("%X"))
+print(datetime.datetime.now().strftime("%A"))
+
---
+++ module/built-in-modules/math.py
@@ -0,0 +1,5 @@
+import math
+print(math.factorial(5))
+print(math.floor(5.5))
+print(math.ceil(5.5))
+print(math.sqrt(64))

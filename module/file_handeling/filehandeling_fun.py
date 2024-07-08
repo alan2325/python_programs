94675e9af4be34c19d51d8ef89cdbf035c3cdaaa
alan2325 <alalkumar06@gmail.com>
2024-07-08 11:12:43 +0530

    done

--- module/file_handeling/filehandeling_fun.py
+++ module/file_handeling/filehandeling_fun.py
@@ -1,2 +1,11 @@
-f=open('python_programs/module/file_handeling/new1.txt','x')
-f.write('123'+'frurnjrujfefeffefefeufuje')
+            #new
+# f=open('python_programs/module/file_handeling/new1.txt','x')
+# f.write('123'+'frurnjrujfefeffefefeufuje')
+
+            #overrite
+# f=open('python_programs/module/file_handeling/new1.txt','x')
+# f.write('123'+'frurnjrujfefeffefefeufuje')
+
+            #new
+f=open('python_programs/module/file_handeling/new2.txt','w')
+f.write('welcome')

fc29cf3b687daa512ee6524e4215189ca823509b
alan2325 <alalkumar06@gmail.com>
2024-07-08 10:34:52 +0530

    done

--- module/built-in-modules/date_time.py
+++ module/built-in-modules/date_time.py
@@ -1,5 +1,5 @@
 import datetime
 print(datetime.datetime.now().strftime("%x"))
-print(datetime.datetime.now().strftime("%X"))
-print(datetime.datetime.now().strftime("%A"))
+# print(datetime.datetime.now().strftime("%X"))
+# print(datetime.datetime.now().strftime("%A"))
 

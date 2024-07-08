2c2727d378fb84924124d24a3867586e09805da2
alan2325 <alalkumar06@gmail.com>
2024-07-08 11:22:38 +0530

    done

--- module/file_handeling/filehandeling_fun.py
+++ module/file_handeling/filehandeling_fun.py
@@ -7,5 +7,22 @@
 # f.write('123'+'frurnjrujfefeffefefeufuje')
 
             #new
-f=open('python_programs/module/file_handeling/new2.txt','w')
-f.write('welcome')
+# f=open('python_programs/module/file_handeling/new2.txt','w')
+# f.write('welcome')
+
+
+
+
+
+            #enter names
+# f=open('python_programs/module/file_handeling/names.txt','x')
+# f.write('alal'+'bla','sreeraj')
+
+            #or
+
+# Open the file in 'x' mode to create it (this will raise an error if the file already exists)
+with open('python_programs/module/file_handeling/names.txt', 'x') as f:
+    # Concatenate the strings into a single string
+    data = 'alal' '\n' + 'bla' + '\n' + 'sreeraj'
+    # Write the concatenated string to the file
+    f.write(data)

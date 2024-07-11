efcc5dfc8189797333f197829500e189486255f3
alan2325 <alalkumar06@gmail.com>
2024-07-11 10:31:10 +0530

    done

--- module/file_handling/filehandeling_fun.py
+++ module/file_handling/filehandeling_fun.py
@@ -20,9 +20,45 @@
 
             #or
 
-l=int(input("Enter limit : "))
-d=[]
-for d in range(l):
-    name=str(input("Enter names : "))
-    f= open('python_programs/module/file_handeling/names.txt', 'x')    
-    f.write(name)
+# l=int(input("Enter limit : "))
+# d=[]
+# for d in range(l):
+#     name=str(input("Enter names : "))
+#     f= open('python_programs/module/file_handeling/names.txt', 'x')    
+#     f.write(name)
+
+
+
+    #append
+# f=open("python_programs/module/file_handling/new.txt","a")
+# f.write("python")
+
+    #read
+# f=open("python_programs/module/file_handling/new.txt","r")
+# print(f.read())
+
+
+    #or
+# f=open("python_programs/module/file_handling/new.txt","r")
+# print(f.read(3))
+# print('_'*20)
+# print(f.read(4))
+
+    #or
+# f=open("python_programs/module/file_handling/new.txt","r")
+# print(f.readline(3))
+# print('_'*20)
+# print(f.readline(4))
+# print(f.readline())
+
+
+    #to count no:lines
+# f=open("python_programs/module/file_handling/new.txt","r")
+# print(f.readlines())
+
+f=open("python_programs/module/file_handling/new.txt","r")
+l=len(f.readlines())
+f.seek(0)
+for i in range(l):
+    a=f.readline().strip()
+    print(a[::-1])

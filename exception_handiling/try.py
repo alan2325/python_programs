a88635be6be356d14f76fc81bc919130a9b09cfa
alan2325 <alalkumar06@gmail.com>
2024-07-12 09:58:37 +0530

    done

--- exception_handiling/try.py
+++ exception_handiling/try.py
@@ -1,5 +1,7 @@
-try :
-    a=int(input("Enter a number : "))
-    print(a)
-except :
-    print("hello")
+while True:
+    try :
+        a=int(input("Enter a number : "))
+        print(a)
+        break
+    except :
+        print("Invalid input !")

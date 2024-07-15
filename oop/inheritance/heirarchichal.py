c7266d349a12c10543bc4be055e7ec627d7489ea
alan2325 <alalkumar06@gmail.com>
2024-07-15 10:38:29 +0530

    done

---
+++ oop/inheritance/heirarchichal.py
@@ -0,0 +1,27 @@
+class synnefo:
+    def __init__(s):
+        print('Regester')
+    def python(s):
+        s.database='mysql'
+        print('python')
+    def php(self):
+        self.database='mysql'
+        print('php',self.database)
+class novavi(synnefo):
+    def dm(self):
+        print('dm')
+    def web(self):
+        print('web developer')
+class std(synnefo):
+    def helo(self):
+        print('helo')       
+
+anu=synnefo()
+anu.php()
+
+bla=novavi()
+bla.dm()
+
+
+aaa=std()
+aaa.helo()

3fb6d741958f7d9f05b5be0cf97dad2b9b026ee9
alan2325 <alalkumar06@gmail.com>
2024-07-18 11:26:22 +0530

    done

--- oop/polymorphism/runtime.py
+++ oop/polymorphism/runtime.py
@@ -1,15 +1,39 @@
+# class sapmle:
+#     def display(self):
+#         print('sample class')
+#     def s1(self):
+#         print('sample class s1')
+# class demo(sapmle):
+#     def display(self):
+#         print('demo display')
+#         super().display()
+#         print('start')
+#     def d1(self):
+#         print('demo class d1')
+    
+# obj=demo()
+# obj.display()
+
+
+
+
+
+    #input name & age
+
 class sapmle:
-    def display(self):
+    def __init__(self,name,age):
         print('sample class')
+        print(name,age)
     def s1(self):
         print('sample class s1')
 class demo(sapmle):
-    def display(self):
+    def __init__(self,name,age):
+        print(name,age)
         print('demo display')
-        super()
+
+        super().__init__(name,age)
         print('start')
     def d1(self):
         print('demo class d1')
     
-obj=demo()
-obj.display()
+obj=demo('akhil',17)

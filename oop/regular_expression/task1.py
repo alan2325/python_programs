48d02c342bf8508693070ef07ec6672513534b62
alan2325 <alalkumar06@gmail.com>
2024-07-19 11:16:23 +0530

    done

--- oop/regular_expression/task1.py
+++ oop/regular_expression/task1.py
@@ -1,4 +1,35 @@
 a='welcome'
 import re
-print(re.sub('w','s',a))
-print(re.split('e',a,1))
+
+        #substitute
+# print(re.sub('w','s',a))
+# 
+# 
+#       #splite
+# # print(re.split('e',a,1))
+
+
+
+
+        #serch
+#1
+# print(re.search('w',a))
+
+#2
+# if re.search('[a-b]',a):
+#     print('yes')
+# else:
+#     print('no')
+
+a='abcd'
+#3
+print(re.search('a..',a))
+
+#4
+print(re.search('b.*',a))
+
+#5
+print(re.search('b.?',a))
+
+#6
+print(re.search('c.+',a))

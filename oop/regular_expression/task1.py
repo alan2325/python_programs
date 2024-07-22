1459b5624de0bf3448c8308f7a67ce97e38e882f
alan2325 <alalkumar06@gmail.com>
2024-07-22 10:11:59 +0530

    done

--- oop/regular_expression/task1.py
+++ oop/regular_expression/task1.py
@@ -21,30 +21,43 @@ import re
 # else:
 #     print('no')
 
-a='abcd'
-#3
-print(re.search('a..',a))
+# a='abcd'
+# #3
+# print(re.search('a..',a))
 
-#4
-print(re.search('b.*',a))
+# #4
+# print(re.search('b.*',a))
 
-#5
-print(re.search('b.?',a))
+# #5
+# print(re.search('b.?',a))
 
-#6
-print(re.search('c.+',a))
+# #6
+# print(re.search('c.+',a))
 
-#7
-print(re.search('[a-z].',a))
+# #7
+# print(re.search('[a-z].',a))
 
-#8
-print(re.search('[a-z].+',a))
+# #8
+# print(re.search('[a-z].+',a))
 
-#9
-print(re.search('[a-z].*',a))
+# #9
+# print(re.search('[a-z].*',a))
 
-#10
-print(re.search('[A-Z].?',a))
+# #10
+# print(re.search('[A-Z].?',a))
 
-#11
-print(re.search('[A-Z]..',a))
+# #11
+# print(re.search('[A-Z]..',a))
+
+
+a='Aabcd123'
+
+# #12
+# print(re.search('[a-z]',a))
+# print(re.search('[A-Z][a-z].*[1-9]',a))
+
+#13 or fun
+print(re.search('a-zA-Z0-9',a))
+
+#14 
+print(re.search('3$',a))

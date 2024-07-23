f870e8a6886b0b6d95315f109ce989cf98f4a6b1
alan2325 <alalkumar06@gmail.com>
2024-07-23 11:21:01 +0530

    done

--- database/task2.py
+++ database/task2.py
@@ -2,18 +2,38 @@
 
 import sqlite3
 
-con = sqlite3.connect("task1.db")
+con = sqlite3.connect("task1.db")#db connection
 
 try:
-    con.execute("create table student(age int,name text,mark real)")
+    con.execute("create table student(age int,name text,mark real)")#create table
 except:
     pass
 
+# l= int (input("Enter limit : "))
+# for i in range(l):
+#     print("------------------------")
+#     age =int(input("Enter age : "))
+#     name= str(input("Enter name : "))
+#     mark = int(input("Enter mark : "))
 
-age =int(input("Enter age : "))
-name= str(input("Enter name : "))
-mark = int(input("Enter mark : "))
+
+#     con.execute("insert into student(age,name,mark) values(?,?,?)",(age,name,mark))#add value
+#     con.commit()#save
 
 
-con.execute("insert into student(age,name,mark) values(?,?,?)",(age,name,mark))
-con.commit()
+
+
+data=con.execute("select * from student ")
+print(data)
+# for i in data:
+#     print(i)
+
+
+
+
+
+print("{:<15}{:<15}{:<15}".format("age","name","mark"))
+print('_'*45)
+for i in data:
+    print("{:<15}{:<15}{:<15}".format(i[0],i[1],i[2])) 
+print()
